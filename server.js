9a0e545f5dac2b3e58201e7fce8c14e685e18d4d
Devarshi Hazarika <49090172+deva24@users.noreply.github.com>
2020-04-12 23:48:43 +0530

    ServerSide Impelemented

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const options = require('./webpack.config');
-const webpack = require("webpack");
+import options from './webpack.config';
+import webpack from 'webpack';
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import StarterApp from './src/App';
+
+import React from 'react';
+import ReactDomServer from 'react-dom/server'
+
 const compiler = webpack(options);
-const express = require('express');
 const app = express();
-const path = require('path');
-const fs = require('fs');
 
 console.log("Compiling...");
 compiler.run((err, stat) =>
 {
     if (err)
-        console.error("Error in compilation " + err);   
+        console.error("Error in compilation " + err);
     else
     {
         console.log("Compilation complete.");
@@ -24,11 +29,11 @@ compiler.run((err, stat) =>
     }
 })
 
-app.use((req,res,nxt)=>
+app.use((req, res, nxt) =>
 {
     console.log("Requested : " + req.url);
 
-    if(req.url==='/dist/bundle.js')
+    if (req.url === '/dist/bundle.js')
     {
         let resolvedPath = path.resolve(__dirname, "dist/bundle.js")
         console.log("Sent : " + resolvedPath);
@@ -36,27 +41,41 @@ app.use((req,res,nxt)=>
         return;
     }
 
-    let url1 = 'public'+req.url;
-    let resolvedPath = path.resolve(__dirname,url1);
+    let url1 = 'public' + req.url;
+    let resolvedPath = path.resolve(__dirname, url1);
 
-    fs.stat(resolvedPath,(err,stat)=>
+    fs.stat(resolvedPath, (err, stat) =>
     {
-        if(err)
+        if (err)
         {
             // if no file then send the usual index.html
-            resolvedPath = path.resolve(__dirname,'public/index.html');
-            console.log('Sent : ' + resolvedPath);
-            res.sendFile(resolvedPath);
+            resolvedPath = path.resolve(__dirname, 'public/index.html');
+            fs.readFile(resolvedPath, { encoding: "UTF8" }, (err, data) =>
+            {
+                if (err)
+                {
+                    throw err;
+                }
+                console.log('Sending renderedFile');
+                res.setHeader('Content-Type', 'text/html');
+                let renderedReactApp = ReactDomServer.renderToString(<StarterApp/>);
+                data = data.replace('<div id="root"></div>', `<div id="root">${renderedReactApp}</div>`);
+                
+                console.log(`ReactApp '${renderedReactApp}'`);
+
+                res.end(data);
+            });
+
             return;
         }
-        
-        if(stat.isDirectory())
+
+        if (stat.isDirectory())
         {
-            resolvedPath = path.resolve(resolvedPath,"index.html");
+            resolvedPath = path.resolve(resolvedPath, "index.html");
             console.log("Sent : " + resolvedPath);
             res.sendFile(resolvedPath);
         }
-        else if(stat.isFile())
+        else if (stat.isFile())
         {
             console.log("Sent : " + resolvedPath);
             res.sendFile(resolvedPath);
--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import {BrowserRouter as Router, Redirect, Link, Switch, Route} from "react-router-dom"
+import { StaticRouter as Router, Redirect, Link, Switch, Route } from "react-router-dom"
 
 function k()
 {
     return <Router>
         Hello world
-        <Link to="/home">Home</Link>
-        <Link to="/about">About</Link>
+        <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+        </ul>
         <Switch>
-            <Route path='/' exact><Redirect to='/home'/></Route>
-            <Route path='/home' exact>Home</Route>
-            <Route path="/about">About</Route>
+            <Route path='/' exact><Redirect to='/home' /></Route>
+            <Route path='/home' exact><div>Home</div></Route>
+            <Route path="/about"><div>About</div></Route>
         </Switch>
     </Router>
+}
+
+function simple()
+{
+    return <div>Hello World</div>;
 }
 export default k;
